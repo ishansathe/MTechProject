50ca4ab70ed07d9f0ed0c32083d5407f576d714a
unknown <ishansatheprofessional@gmail.com>
2024-07-04 20:37:00 +0530

    The alternative to the FrontRunning code. Can also be exploited with frontrunning

---
+++ contracts/FrontRun/Griefer.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.17;
+
+// Reference: https://scsfg.io/hackers/griefing/
+
+contract DelayedWithdrawal {
+    address beneficiary;
+    uint256 delay;
+    uint256 lastDeposit;
+
+    constructor(uint256 _delay) {
+        beneficiary = msg.sender;
+        lastDeposit = block.timestamp;
+        delay = _delay;
+    }
+
+    modifier checkDelay() {
+        require(block.timestamp >= lastDeposit + delay, "Keep waiting");
+        _;
+    }
+
+    function deposit() public payable {
+        require(msg.value != 0);
+        lastDeposit = block.timestamp;
+    }
+
+    function withdraw() public checkDelay {
+        (bool success, ) = beneficiary.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+}
